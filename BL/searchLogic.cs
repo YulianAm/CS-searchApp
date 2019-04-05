d7e7ba98295da4702ac6ff36f56b8bec1d2dbd2d
yuliantau@gmail.com <yuliantau@gmail.com>
2019-04-05 11:04:31 +0300

    first

---
+++ BL/searchLogic.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using DAL;
+
+namespace BL
+{
+
+  
+
+
+
+    public static class Search
+    {
+
+
+        public static void addPathDB(string resultFilePath, string searchTermUser, string searchPathUser, DateTime timeStamp, string searchType)
+        {
+
+            var ent = SearchDB.ent;
+
+
+            searchLog t = new searchLog()
+            {
+                searchTerm = searchTermUser,
+                searchPath = searchPathUser,
+                resultFilePath = resultFilePath,
+                DateSearched = timeStamp,
+                SearchType = searchType
+            };
+
+            try
+            {
+                ent.searchLogs.Add(t);
+                ent.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw ;
+            }
+            
+
+
+
+
+        }
+
+        public static IEnumerable<string> SearchFiles(string root, string searchTerm)
+        {
+
+            var files = new List<string>();
+            var messages = new List<string>();
+            var results = files.Concat(messages);
+
+            try
+            {
+                IEnumerable<string> listfiles = Directory.EnumerateFiles(root).Select(e => e.ToLower()).
+                    Where(m => m.Contains(searchTerm.ToLower()));
+                //add paths conatining the file to lsit 'files' - per run 
+
+                foreach (var path in listfiles)
+                {
+                    var directory = Path.GetDirectoryName(path);
+
+                    
+
+                    //test to see if the file found is folder or not - we do not want folders to display and logged ONLY files!
+                    if (Path.GetFileName(path).Contains(searchTerm))
+                    {
+                        
+                        files.Add(path);
+                    }
+
+
+                    // including folders
+
+                    //if (directory.Contains(searchTerm))
+                    //{
+                    //    Boolean isDirAlreadyInList = false;
+
+
+
+                    //    foreach (var file in files)
+                    //    {
+                    //        if (file.Contains(directory))
+                    //        {
+                    //            isDirAlreadyInList = true;
+                    //            break;
+                    //        }
+                    //        else { continue; }
+
+
+                    //    }
+
+                    //    if (!isDirAlreadyInList) { files.Add(directory);  }
+
+                    //}
+
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+                messages.Add("no files found!");
+
+
+
+            }
+
+
+            try
+            {
+                //next run to search inside existing path folder (go one step deeper)
+                foreach (var subDir in Directory.EnumerateDirectories(root))
+                {
+                    try
+                    {
+                        files.AddRange(SearchFiles(subDir, searchTerm));
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        messages.Add("no access to required folder:" + ex.Message + " manage permissions and make sure you're admin");
+                        addPathDB("N/A", searchTerm, root, DateTime.Now, "search term + path - no access");
+                        
+                        
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException dirEx)
+            {
+                messages.Add("Directory not found: " + dirEx.Message);
+                addPathDB("N/A", searchTerm, root, DateTime.Now, "search term + path - broken path");
+            }
+
+
+            return results;
+
+        }
+    }
+}
+
---
+++ SearchApp/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static BL.Search;
+using BL;
+using System.Threading;
+
+namespace SearchApp
+{
+    class Program
+    {
+
+
+        public static void printMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine("Welcome to search app!");
+            Console.WriteLine("1. Enter file name to search");
+            Console.WriteLine("2. Enter file name to search + parent directory to search in ");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+        }
+
+        public static bool validateNotNull(string userInput)
+        {
+
+            if (string.IsNullOrEmpty(userInput)) { Console.WriteLine("empty user input!"); return true; } else { return false; }
+
+
+        }
+
+        public static void swithcUserInput(string input)
+        {
+            Boolean isEmptyInput;
+            string defaultDirectory = "c:\\test\\";
+            string userInput;
+            IEnumerable<string> lines;
+            DateTime nowTime = DateTime.Now;
+
+
+            void insertResultsToDataBaseNameOnly(IEnumerable<string> userLines)
+            {
+                if (userLines.Count() != 0)
+                {
+                    foreach (var line in userLines)
+                    {
+                        addPathDB(line, userInput, defaultDirectory, nowTime, "name only");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("file not found");
+                    addPathDB("N/A", userInput, defaultDirectory, nowTime, "name only");
+                }
+            }
+            void insertResultsToDataBaseNameAndPath(IEnumerable<string> userLines)
+            {
+                if (userLines.Count() != 0)
+                {
+                    foreach (var line in userLines)
+                    {
+                        Search.addPathDB(line, userInput, defaultDirectory, nowTime, "name + path");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("file not found");
+                    Search.addPathDB("N/A", userInput, defaultDirectory, nowTime, "name + path");
+                }
+            }
+            void forEachPrintResult(IEnumerable<string> list)
+            {
+                foreach(var v in list) {
+                    Thread.Sleep(500);
+                    Console.WriteLine(v);
+                }
+
+            }
+
+            switch (input)
+            {
+
+                case "1":
+
+                    Console.WriteLine("Enter file name to search:");
+                    userInput = Console.ReadLine();
+                    //validate input is not 0 or null
+                    isEmptyInput = validateNotNull(userInput);
+                    if (isEmptyInput) break;
+
+                    Console.WriteLine("searching in folder: {0} searching for term: {1}.....", defaultDirectory, userInput);
+                    lines = SearchFiles(defaultDirectory, userInput);
+                    forEachPrintResult(lines);
+                    insertResultsToDataBaseNameOnly(lines);
+                    break;
+
+                case "2":
+
+                    Console.WriteLine("Enter file name to search:");
+                    userInput = Console.ReadLine();
+
+                    //validate search term
+                    isEmptyInput = validateNotNull(userInput);
+                    if (isEmptyInput) break;
+
+
+                    Console.WriteLine("Enter parent directory to search in:");
+                    defaultDirectory = Console.ReadLine();
+
+
+                    //validate path
+                    isEmptyInput = validateNotNull(defaultDirectory);
+                    if (isEmptyInput) break;
+
+
+                    Console.WriteLine("searching in folder: {0} searching for term: {1}.....", defaultDirectory, userInput);
+
+                    lines = SearchFiles(defaultDirectory, userInput);
+                    forEachPrintResult(lines);
+                    insertResultsToDataBaseNameAndPath(lines);
+                    break;
+
+                case "3":
+                    Console.WriteLine("goodbye!");
+                    Thread.Sleep(1000);
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("invalid input - please try again");
+                    break;
+
+            }
+
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+        
+            string userInput;
+
+            
+
+            while(true)
+            {
+                printMenu();
+                userInput = Console.ReadLine();
+
+
+                //switch user choice -> switch case
+                swithcUserInput(userInput);
+
+                if ( userInput == "3" ) break; continue;
+
+                
+
+            }
+            
+
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
